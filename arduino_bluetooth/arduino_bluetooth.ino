0688a39a9af7f090a293612dba6c19027196f517
escri11 <escri11@yahoo.es>
2017-05-10 17:59:21 +0200

    Bluetooth per obrir i tancar el rele

--- arduino_bluetooth/arduino_bluetooth.ino
+++ arduino_bluetooth/arduino_bluetooth.ino
@@ -13,19 +13,22 @@ SoftwareSerial BTserial(2, 3); // Rx and Tx of your Arduino
 
 char c[100];
 int i = 0;
+int relay = 10;
 bool input = false;
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Arduino is ready");
-  Serial.println("Remember to select Both NL & CR in the serial monitor");
+  Serial.println("Remember to select Both NL & CR i  n the serial monitor");
   BTserial.begin(9600);
+  pinMode(relay, OUTPUT);
 
 
-  
+
+
 }
 void loop()
-{   
+{
     delay(500);
     if (BTserial.available()){
     input = true;
@@ -37,26 +40,25 @@ void loop()
 
  if(input){
 
- // We write through our Arduino Serial port (USB connected) what we sent. 
+ // We write through our Arduino Serial port (USB connected) what we sent.
  Serial.println("Input detected, you sent:");
  Serial.println(c);
 
- // We write to out mobile phone 
+ // We write to out mobile phone
  BTserial.print("You sent: ");
  BTserial.println(c);
 
  //We see what can we do with our data
  if(!strcmp(c,"on")){
   //do something
-  Serial.println("Shots fired");
-  BTserial.println("Shots fired");
+  digitalWrite(relay, LOW);
+  }
+  else if (!strcmp(c,"off")) {
+    digitalWrite(relay, HIGH);
   }
  //reset variables
- input = false; 
+ input = false;
  i = 0;
  memset(c,0, sizeof(c));
  }
 }
-
-
-
