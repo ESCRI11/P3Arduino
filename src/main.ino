5de95370a724d7b140ea9e66fd86430ae11eabc3
escri11 <escri11@yahoo.es>
2017-05-10 21:33:49 +0200

    main update

--- src/main.ino
+++ src/main.ino
@@ -1,14 +1,12 @@
 int sortida = 8;
 
-void setup() {
-  pinMode(sortida, OUTPUT);
-}
+void setup() {}
 
 
 void loop() {
   digitalWrite(sortida, HIGH);
   delay(150);
-  digitalWrite(sortida, LOW);   
+  digitalWrite(sortida, LOW);
   delay(350);
   digitalWrite(sortida, HIGH);
   delay(550);
